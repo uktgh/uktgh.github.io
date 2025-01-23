da0ea761473c1d507a391021bcc9c40d2a9816c5
Vittorio <159624128+uktgh@users.noreply.github.com>
2025-01-23 15:02:04 +0100

    - changed config

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -139,7 +139,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'WireFrame',
+    defaultTheme: 'pastel',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
